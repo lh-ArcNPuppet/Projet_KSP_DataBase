d30afa0382d67ccad624d8e4679d1f71fb4be9c2
ArcNPuppet (Louis.H) <161235050+lh-ArcNPuppet@users.noreply.github.com>
2025-05-08 11:48:19 +0200

    + Ajout du formulaire "Dashboard" avec un MenuStrip pour accéder aux différents formulaires de la solution.

---
+++ Dashboard/frDashboard.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace Dashboard
+{
+    partial class frDashboard
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.donnéesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.missionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lanceursToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.payloadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kerbonautesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.outilsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.paramètresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.calculateurDeDeltaVToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.calculateurLKOToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.configMissionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.configLanceurToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.configPayloadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.configKerbonauteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editerLaListeDesStatusMissionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editerLaListeDesSituationsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editerLaListeDesTypesDeLanveursToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editerLaListeDesTypesDePayloadsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editerLaListeDesProfessionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.donnéesToolStripMenuItem,
+            this.outilsToolStripMenuItem,
+            this.paramètresToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // donnéesToolStripMenuItem
+            // 
+            this.donnéesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.missionsToolStripMenuItem,
+            this.lanceursToolStripMenuItem,
+            this.payloadToolStripMenuItem,
+            this.kerbonautesToolStripMenuItem});
+            this.donnéesToolStripMenuItem.Name = "donnéesToolStripMenuItem";
+            this.donnéesToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.donnéesToolStripMenuItem.Text = "Données";
+            // 
+            // missionsToolStripMenuItem
+            // 
+            this.missionsToolStripMenuItem.Name = "missionsToolStripMenuItem";
+            this.missionsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.missionsToolStripMenuItem.Text = "Missions";
+            // 
+            // lanceursToolStripMenuItem
+            // 
+            this.lanceursToolStripMenuItem.Name = "lanceursToolStripMenuItem";
+            this.lanceursToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.lanceursToolStripMenuItem.Text = "Lanceurs";
+            // 
+            // payloadToolStripMenuItem
+            // 
+            this.payloadToolStripMenuItem.Name = "payloadToolStripMenuItem";
+            this.payloadToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.payloadToolStripMenuItem.Text = "Payloads";
+            // 
+            // kerbonautesToolStripMenuItem
+            // 
+            this.kerbonautesToolStripMenuItem.Name = "kerbonautesToolStripMenuItem";
+            this.kerbonautesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.kerbonautesToolStripMenuItem.Text = "Kerbonautes";
+            // 
+            // outilsToolStripMenuItem
+            // 
+            this.outilsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.calculateurDeDeltaVToolStripMenuItem,
+            this.calculateurLKOToolStripMenuItem});
+            this.outilsToolStripMenuItem.Name = "outilsToolStripMenuItem";
+            this.outilsToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.outilsToolStripMenuItem.Text = "Outils";
+            // 
+            // paramètresToolStripMenuItem
+            // 
+            this.paramètresToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.configMissionToolStripMenuItem,
+            this.configLanceurToolStripMenuItem,
+            this.configPayloadToolStripMenuItem,
+            this.configKerbonauteToolStripMenuItem});
+            this.paramètresToolStripMenuItem.Name = "paramètresToolStripMenuItem";
+            this.paramètresToolStripMenuItem.Size = new System.Drawing.Size(78, 20);
+            this.paramètresToolStripMenuItem.Text = "Paramètres";
+            // 
+            // calculateurDeDeltaVToolStripMenuItem
+            // 
+            this.calculateurDeDeltaVToolStripMenuItem.Name = "calculateurDeDeltaVToolStripMenuItem";
+            this.calculateurDeDeltaVToolStripMenuItem.Size = new System.Drawing.Size(190, 22);
+            this.calculateurDeDeltaVToolStripMenuItem.Text = "Calculateur de Delta V";
+            // 
+            // calculateurLKOToolStripMenuItem
+            // 
+            this.calculateurLKOToolStripMenuItem.Name = "calculateurLKOToolStripMenuItem";
+            this.calculateurLKOToolStripMenuItem.Size = new System.Drawing.Size(190, 22);
+            this.calculateurLKOToolStripMenuItem.Text = "Calculateur LKO";
+            // 
+            // configMissionToolStripMenuItem
+            // 
+            this.configMissionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.editerLaListeDesStatusMissionToolStripMenuItem,
+            this.editerLaListeDesSituationsToolStripMenuItem});
+            this.configMissionToolStripMenuItem.Name = "configMissionToolStripMenuItem";
+            this.configMissionToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.configMissionToolStripMenuItem.Text = "Config Mission";
+            // 
+            // configLanceurToolStripMenuItem
+            // 
+            this.configLanceurToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.editerLaListeDesTypesDeLanveursToolStripMenuItem});
+            this.configLanceurToolStripMenuItem.Name = "configLanceurToolStripMenuItem";
+            this.configLanceurToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.configLanceurToolStripMenuItem.Text = "Config Lanceur";
+            // 
+            // configPayloadToolStripMenuItem
+            // 
+            this.configPayloadToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.editerLaListeDesTypesDePayloadsToolStripMenuItem});
+            this.configPayloadToolStripMenuItem.Name = "configPayloadToolStripMenuItem";
+            this.configPayloadToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.configPayloadToolStripMenuItem.Text = "Config Payload";
+            // 
+            // configKerbonauteToolStripMenuItem
+            // 
+            this.configKerbonauteToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.editerLaListeDesProfessionsToolStripMenuItem});
+            this.configKerbonauteToolStripMenuItem.Name = "configKerbonauteToolStripMenuItem";
+            this.configKerbonauteToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.configKerbonauteToolStripMenuItem.Text = "Config Kerbonaute";
+            // 
+            // editerLaListeDesStatusMissionToolStripMenuItem
+            // 
+            this.editerLaListeDesStatusMissionToolStripMenuItem.Name = "editerLaListeDesStatusMissionToolStripMenuItem";
+            this.editerLaListeDesStatusMissionToolStripMenuItem.Size = new System.Drawing.Size(239, 22);
+            this.editerLaListeDesStatusMissionToolStripMenuItem.Text = "Editer la liste des status mission";
+            // 
+            // editerLaListeDesSituationsToolStripMenuItem
+            // 
+            this.editerLaListeDesSituationsToolStripMenuItem.Name = "editerLaListeDesSituationsToolStripMenuItem";
+            this.editerLaListeDesSituationsToolStripMenuItem.Size = new System.Drawing.Size(239, 22);
+            this.editerLaListeDesSituationsToolStripMenuItem.Text = "Editer la liste des situations";
+            // 
+            // editerLaListeDesTypesDeLanveursToolStripMenuItem
+            // 
+            this.editerLaListeDesTypesDeLanveursToolStripMenuItem.Name = "editerLaListeDesTypesDeLanveursToolStripMenuItem";
+            this.editerLaListeDesTypesDeLanveursToolStripMenuItem.Size = new System.Drawing.Size(255, 22);
+            this.editerLaListeDesTypesDeLanveursToolStripMenuItem.Text = "Editer la liste des types de lanceurs";
+            // 
+            // editerLaListeDesTypesDePayloadsToolStripMenuItem
+            // 
+            this.editerLaListeDesTypesDePayloadsToolStripMenuItem.Name = "editerLaListeDesTypesDePayloadsToolStripMenuItem";
+            this.editerLaListeDesTypesDePayloadsToolStripMenuItem.Size = new System.Drawing.Size(258, 22);
+            this.editerLaListeDesTypesDePayloadsToolStripMenuItem.Text = "Editer la liste des types de payloads";
+            // 
+            // editerLaListeDesProfessionsToolStripMenuItem
+            // 
+            this.editerLaListeDesProfessionsToolStripMenuItem.Name = "editerLaListeDesProfessionsToolStripMenuItem";
+            this.editerLaListeDesProfessionsToolStripMenuItem.Size = new System.Drawing.Size(224, 22);
+            this.editerLaListeDesProfessionsToolStripMenuItem.Text = "Editer la liste des professions";
+            // 
+            // frDashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frDashboard";
+            this.Text = " ";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem donnéesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem missionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lanceursToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem payloadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kerbonautesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem outilsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem calculateurDeDeltaVToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem calculateurLKOToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem paramètresToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem configMissionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem configLanceurToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem configPayloadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem configKerbonauteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editerLaListeDesStatusMissionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editerLaListeDesSituationsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editerLaListeDesTypesDeLanveursToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editerLaListeDesTypesDePayloadsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editerLaListeDesProfessionsToolStripMenuItem;
+    }
+}
+
--- Dashboard/frDashboard.cs
+++ Dashboard/frDashboard.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Dashboard
 {
-    public partial class Form1: Form
+    public partial class frDashboard: Form
     {
-        public Form1()
+        public frDashboard()
         {
             InitializeComponent();
         }
